a715007d1bb0f5c314f4f04a1146cfd04a26f431
facudegani-max <facudegani@gmail.com>
2025-10-22 00:01:03 -0300

    Agregar archivos de proyecto.

---
+++ pryDeganiSp3.1/frmMain.Designer.cs
@@ -0,0 +1,277 @@
+﻿namespace pryDeganiSp3._1
+{
+    partial class frmMain
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            txtNumTurno = new TextBox();
+            txtDominio = new TextBox();
+            txtTitulo = new TextBox();
+            numericUpDown1 = new NumericUpDown();
+            label5 = new Label();
+            label6 = new Label();
+            label7 = new Label();
+            txtCantidadTurnos = new TextBox();
+            txtAnioAntiguo = new TextBox();
+            txtCantidadCaracter = new TextBox();
+            btnRegistrar = new Button();
+            btnSalir = new Button();
+            btnConsultar = new Button();
+            grpRegistro = new GroupBox();
+            grpEstadisticas = new GroupBox();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown1).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(34, 36);
+            label1.Name = "label1";
+            label1.Size = new Size(126, 20);
+            label1.TabIndex = 0;
+            label1.Text = "Numero de Turno";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(34, 69);
+            label2.Name = "label2";
+            label2.Size = new Size(67, 20);
+            label2.TabIndex = 1;
+            label2.Text = "Dominio";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(34, 102);
+            label3.Name = "label3";
+            label3.Size = new Size(115, 20);
+            label3.TabIndex = 2;
+            label3.Text = "Año Fabricacion";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(34, 135);
+            label4.Name = "label4";
+            label4.Size = new Size(47, 20);
+            label4.TabIndex = 3;
+            label4.Text = "Titulo";
+            // 
+            // txtNumTurno
+            // 
+            txtNumTurno.Location = new Point(178, 33);
+            txtNumTurno.MaxLength = 5;
+            txtNumTurno.Name = "txtNumTurno";
+            txtNumTurno.Size = new Size(72, 27);
+            txtNumTurno.TabIndex = 4;
+            // 
+            // txtDominio
+            // 
+            txtDominio.Location = new Point(178, 66);
+            txtDominio.MaxLength = 7;
+            txtDominio.Name = "txtDominio";
+            txtDominio.Size = new Size(125, 27);
+            txtDominio.TabIndex = 5;
+            // 
+            // txtTitulo
+            // 
+            txtTitulo.Location = new Point(178, 132);
+            txtTitulo.MaxLength = 30;
+            txtTitulo.Name = "txtTitulo";
+            txtTitulo.Size = new Size(301, 27);
+            txtTitulo.TabIndex = 6;
+            // 
+            // numericUpDown1
+            // 
+            numericUpDown1.Location = new Point(178, 99);
+            numericUpDown1.Maximum = new decimal(new int[] { 2025, 0, 0, 0 });
+            numericUpDown1.Minimum = new decimal(new int[] { 1950, 0, 0, 0 });
+            numericUpDown1.Name = "numericUpDown1";
+            numericUpDown1.Size = new Size(84, 27);
+            numericUpDown1.TabIndex = 7;
+            numericUpDown1.Value = new decimal(new int[] { 1950, 0, 0, 0 });
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(34, 228);
+            label5.Name = "label5";
+            label5.Size = new Size(138, 20);
+            label5.TabIndex = 8;
+            label5.Text = "Cantidad de Turnos";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(34, 263);
+            label6.Name = "label6";
+            label6.Size = new Size(147, 20);
+            label6.TabIndex = 9;
+            label6.Text = "Año del mas antiguo";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(34, 298);
+            label7.Name = "label7";
+            label7.Size = new Size(188, 20);
+            label7.TabIndex = 10;
+            label7.Text = "Dominios con 6 caracteres ";
+            // 
+            // txtCantidadTurnos
+            // 
+            txtCantidadTurnos.Location = new Point(228, 225);
+            txtCantidadTurnos.Name = "txtCantidadTurnos";
+            txtCantidadTurnos.ReadOnly = true;
+            txtCantidadTurnos.Size = new Size(106, 27);
+            txtCantidadTurnos.TabIndex = 11;
+            // 
+            // txtAnioAntiguo
+            // 
+            txtAnioAntiguo.Location = new Point(228, 260);
+            txtAnioAntiguo.Name = "txtAnioAntiguo";
+            txtAnioAntiguo.ReadOnly = true;
+            txtAnioAntiguo.Size = new Size(106, 27);
+            txtAnioAntiguo.TabIndex = 12;
+            // 
+            // txtCantidadCaracter
+            // 
+            txtCantidadCaracter.Location = new Point(228, 295);
+            txtCantidadCaracter.Name = "txtCantidadCaracter";
+            txtCantidadCaracter.ReadOnly = true;
+            txtCantidadCaracter.Size = new Size(106, 27);
+            txtCantidadCaracter.TabIndex = 13;
+            // 
+            // btnRegistrar
+            // 
+            btnRegistrar.BackColor = Color.IndianRed;
+            btnRegistrar.Location = new Point(506, 33);
+            btnRegistrar.Name = "btnRegistrar";
+            btnRegistrar.Size = new Size(115, 43);
+            btnRegistrar.TabIndex = 14;
+            btnRegistrar.Text = "Registrar";
+            btnRegistrar.UseVisualStyleBackColor = false;
+            btnRegistrar.Click += this.btnRegistrar_Click;
+            // 
+            // btnSalir
+            // 
+            btnSalir.BackColor = Color.IndianRed;
+            btnSalir.Location = new Point(506, 90);
+            btnSalir.Name = "btnSalir";
+            btnSalir.Size = new Size(115, 43);
+            btnSalir.TabIndex = 15;
+            btnSalir.Text = "Salir";
+            btnSalir.UseVisualStyleBackColor = false;
+            btnSalir.Click += this.button2_Click;
+            // 
+            // btnConsultar
+            // 
+            btnConsultar.BackColor = Color.IndianRed;
+            btnConsultar.Location = new Point(506, 217);
+            btnConsultar.Name = "btnConsultar";
+            btnConsultar.Size = new Size(115, 43);
+            btnConsultar.TabIndex = 16;
+            btnConsultar.Text = "Consultar";
+            btnConsultar.UseVisualStyleBackColor = false;
+            // 
+            // grpRegistro
+            // 
+            grpRegistro.Location = new Point(12, 12);
+            grpRegistro.Name = "grpRegistro";
+            grpRegistro.Size = new Size(478, 174);
+            grpRegistro.TabIndex = 17;
+            grpRegistro.TabStop = false;
+            grpRegistro.Text = "Registro";
+            // 
+            // grpEstadisticas
+            // 
+            grpEstadisticas.Location = new Point(12, 202);
+            grpEstadisticas.Name = "grpEstadisticas";
+            grpEstadisticas.Size = new Size(478, 146);
+            grpEstadisticas.TabIndex = 18;
+            grpEstadisticas.TabStop = false;
+            grpEstadisticas.Text = "Estadisticas";
+            // 
+            // frmMain
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(633, 368);
+            Controls.Add(btnConsultar);
+            Controls.Add(btnSalir);
+            Controls.Add(btnRegistrar);
+            Controls.Add(txtCantidadCaracter);
+            Controls.Add(txtAnioAntiguo);
+            Controls.Add(txtCantidadTurnos);
+            Controls.Add(label7);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(numericUpDown1);
+            Controls.Add(txtTitulo);
+            Controls.Add(txtDominio);
+            Controls.Add(txtNumTurno);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(grpRegistro);
+            Controls.Add(grpEstadisticas);
+            Name = "frmMain";
+            Text = "Auto Test";
+            ((System.ComponentModel.ISupportInitialize)numericUpDown1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private TextBox txtNumTurno;
+        private TextBox txtDominio;
+        private TextBox txtTitulo;
+        private NumericUpDown numericUpDown1;
+        private Label label5;
+        private Label label6;
+        private Label label7;
+        private TextBox txtCantidadTurnos;
+        private TextBox txtAnioAntiguo;
+        private TextBox txtCantidadCaracter;
+        private Button btnRegistrar;
+        private Button btnSalir;
+        private Button btnConsultar;
+        private GroupBox grpRegistro;
+        private GroupBox grpEstadisticas;
+    }
+}
---
+++ pryDeganiSp3.1/frmMain.cs
@@ -0,0 +1,10 @@
+namespace pryDeganiSp3._1
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+    }
+}
